4120e2b9b7a04f7570043767161a40589c006cc4
Nacbotics <xavierroymccoy@gmail.com>
2021-11-08 19:20:01 +0100

    Fixed the bug in the code

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = ''# gotten from https://my.telegram.org/
-api_hash = '' # gotten from https://my.telegram.org/
+api_id = 2637893 # gotten from https://my.telegram.org/
+api_hash = "3debb43701b1f4519a07629790e710d7"# gotten from https://my.telegram.org/
 
-phone = ''#your phone number with the country code e.g +23480133558
+phone = '+2349072023313' #your phone number with the country code e.g +23480133558
--- get_members.py
+++ get_members.py
@@ -3,50 +3,13 @@ from config import*
 from telethon import TelegramClient, sync
 
 
-client = TelegramClient(phone, api_id, api_hash)
- 
-# get all the channels that I can access
-channels = {d.entity.username: d.entity
-            for d in client.get_dialogs()
-            if d.is_channel}
+client = TelegramClient(None, api_id, api_hash)
+client.start()
 
-
-def save_members(username,id, access_hash,name):
-    with open('tbot.csv', mode='a+') as csv_file:
-        fieldnames = ['username','id','access_hash','name']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        writer.writeheader()
-        writer.writerow({'username': username,'id':id,'access_hash': access_hash,'name': name})
-
-
-for i,channel_name in enumerate(channels.keys()):
-    try:print(f"{i} ::: {channel_name}")
-    except:pass
-
-channel_id = int(input("Please select a number :: "))
-
-channel_name = list(channels.keys())[channel_id]
-channel = channels[channel_name]
-print(f"\n\nYou have selected:: {channel_name}")
-
-print("\n\nGetting the members of this channel\n")
-
-try:
-    # get all the users and print them
-    for user in client.get_participants(channel):
-        name = f"{user.first_name} {user.last_name}"
-        save_members(user.username,user.id,user.access_hash,name)import csv
-from config import*
-from telethon import TelegramClient, sync
-
-
-client = TelegramClient(phone, api_id, api_hash)
-
-client.connect()
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
+#client.connect()
+# if not client.is_user_authorized():
+#    client.send_code_request(phone)
+#    client.sign_in(phone, input('Enter the code: '))
 
 
 
@@ -69,7 +32,7 @@ def get_channels():
     print("\n\nGetting the members of this channel\n")
 
     try:
-        with open('tbot.csv', mode='w+') as csv_file:
+        with open(f'{channel_name}.csv', mode='w+') as csv_file:
             fieldnames = ['username','id','access_hash','name']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
@@ -78,9 +41,10 @@ def get_channels():
             for user in client.get_participants(channel):
                 name = f"{user.first_name} {user.last_name}"
                 writer.writerow({'username': user.username,'id':user.id,'access_hash':user.access_hash,'name':name})
-                print(user.username,user.id,user.access_hash,name)
+                #print(user.username,user.id,user.access_hash,name)
+            return(f"Users from ({channel_name}) have been copied successfully")
     except Exception as e:
-		    print(e)
+		    print(f"An error occurred because:: {e}")
 		    get_channels()
 		    
 try:
@@ -88,11 +52,4 @@ try:
 except Exception as e:
     print(e)
 		     
-
-
-
-        print(user.username,user.id,user.access_hash,name)
-except Exception as e:
-		print(e)
-
 
